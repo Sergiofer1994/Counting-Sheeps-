2ff0a12ee9e53e4b16f1b7d1e77fcfcbac00a618
Sergiofer1994 <sergiofer1994@gmail.com>
2025-12-03 12:55:52 +0100

    feat: javascrip started

---
+++ index.js
@@ -0,0 +1,15 @@
+
+
+function contador1(list) {
+    let texto1 = document.getElementById("primerContador");
+    let texto2 = document.getElementById("segundoContador");
+    let texto3 = document.getElementById("tercerContador");
+    let list = [true, true, true, false, true, true, true, true, true, false, true, false, true, false, false, true, true, true, true, true, false, false, true, true]
+    let list2 = [false, false, false]
+    let list3 = {}
+    function countSheep(list) {
+        
+    }
+    }
+    
+ 
