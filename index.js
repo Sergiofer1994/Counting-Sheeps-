346763887b867d0585058d6f198ec96729a00582
Sergiofer1994 <sergiofer1994@gmail.com>
2025-12-04 19:35:12 +0100

    feat:js done

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 
 let texto1 = document.getElementById("primerContador");
-let texto2 = document.getElementById("segundoContador");
+let texto2 = document.getElementById("segundoCntador");
 let texto3 = document.getElementById("tercerContador");
-let list = [true, true, true, false, true, true, true, true, true, false, true, false, true, false, false, true, true, true, true, true, false, false, true, true]
-let list2 = [false, false, false]
-let list3 = {}
+let list = [true, true, true, false, true, true, true, true, true, false, true, false, true, false, false, true, true, true, true, true, false, false, true, true];
+let list2 = [false, false, false];
+let list3 = [];
+countSheep(list);
+countSheep(list2);
+countSheep(list3);
 function countSheep(list) {
     let counterSheep = 0;
     let counterWolf = 0;
@@ -17,7 +20,14 @@ function countSheep(list) {
 
         }
     }
-    if((counterSheep==0)&&(counterWolf>0)))){
-    texto2.innerHTML = `"UPS!!! Wolfs eaten Sheeps;
+    if ((counterSheep == 0) && (counterWolf > 0)) {
+        texto2.innerHTML = "UPS!!! Wolfs eaten Sheeps";
+
+    }
+    if (counterSheep > 0) {
+        texto3.innerHTML = "There are " + counterSheep + " sheeps in total";
+    }
+    if ((counterSheep == 0) && (counterWolf == 0)) {
+        texto1.innerHTML = "There are no sheeps or wolfs";
     }
 }
